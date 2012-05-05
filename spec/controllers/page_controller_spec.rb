bb5008b92aaddfebf075db2be6250e99a8192c0b
dondenoncourt <dondenoncourt@192.168.1.6>
2012-05-05 11:49:14 -0400

    post view: format date, show brand, name not description.

--- spec/controllers/page_controller_spec.rb
+++ spec/controllers/page_controller_spec.rb
@@ -1,6 +1,11 @@
 require 'spec_helper'
 
 describe PageController do
+  fixtures :users
+
+  before(:each) do
+    sign_in users(:users_001)
+  end
 
   describe "GET 'home'" do
     it "returns http success" do
