0caf64278b93862aa60de153feb66780dd7597c5
Aaron Bartell <aaronbartell@gmail.com>
2012-05-09 11:50:26 -0500

    Added has_many set_asides so items table could be related to it from this direction (set_asides already had reference to items table from its model file)

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,6 +7,7 @@ class Item < ActiveRecord::Base
   has_many :hashtags, :through => :post, :conditions => ["hashtags.status = 1"]
   has_many :hashtag_values, :through => :hashtags
   has_many :suggestions
+  has_many :set_asides
   belongs_to :parent, :class_name => "Item", :foreign_key => "parent_id", :conditions => ["items.status = 1"]
   
   validates :user_id, :post_id, :presence => true
