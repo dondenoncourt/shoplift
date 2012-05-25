b6c96f9f276e1693269a1996429ca41900c1ef9f
Alfie La Peter <alfie@alfielapeter.com>
2012-05-25 12:42:13 -0600

    layout fix for devise controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   # Return 404 when record is not found
   rescue_from ActiveRecord::RecordNotFound, :with => :render_active_record_error
- 
+
   # Fetch all Room objects with boundaries
   #
   # * *Args*    :
@@ -13,11 +13,11 @@ class ApplicationController < ActionController::Base
     if http_status_code.blank?
       http_status_code = 404
     end
-    
+
     if content.blank?
       case http_status_code
         when 300
-          content = "Multiple Choices" 
+          content = "Multiple Choices"
         when 301
           content = "Moved Permanently"
         when 304
@@ -25,7 +25,7 @@ class ApplicationController < ActionController::Base
         when 307
           content = "Temporary Redirect"
         when 400
-          content = "Bad Request"  
+          content = "Bad Request"
         when 401
           content = "Unauthorized"
         when 403
@@ -35,18 +35,18 @@ class ApplicationController < ActionController::Base
         when 405
           content = "Method Not Allowed"
         when 406
-          content = "Not Acceptable" 
+          content = "Not Acceptable"
         when 410
           content = "Gone"
         when 500
           content = "Internal Server Error"
         # else
           # content = "Not Found"
-      end  
+      end
     end
     render :json => "#{ http_status_code } #{ content }", :status => http_status_code
   end
-  
+
   # Render 404 error when no record founds
   #
   # * *Args*    :
@@ -55,7 +55,7 @@ class ApplicationController < ActionController::Base
   def render_active_record_error
     render :json => "404 Not Found", :status => 404
   end
-  
+
   # Fetch all Room objects with boundaries
   #
   # * *Args*    :
@@ -66,7 +66,11 @@ class ApplicationController < ActionController::Base
     object.errors.full_messages.each do |k, v|
       notice << " | #{k} #{v} "
     end
-    return render_error(406,notice)  
+    return render_error(406,notice)
   end
- 
+
+  layout Proc.new { |controller|
+    nil if controller.request.xhr? || devise_controller?
+  }
+
 end
