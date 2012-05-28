9528cd2007cb3369a83aaf5a58df2329be43349a
Alfie La Peter <alfie@alfielapeter.com>
2012-05-28 16:33:44 -0600

    cleaned up item saving

--- app/controllers/set_asides_controller.rb
+++ app/controllers/set_asides_controller.rb
@@ -1,6 +1,6 @@
 class SetAsidesController < ApplicationController
   before_filter :authenticate_user!
-  
+
   # Fetch all set asides for current user
   # * *Request*    :
   #   - GET /set_asides
@@ -13,37 +13,27 @@ class SetAsidesController < ApplicationController
                           .where("set_asides.user_id = ? AND set_asides.status = 1",current_user.id) \
                           .group("items.id")
   end
-  
+
   # Create set aside
   # * *Request*    :
   #   - POST /set_asides/items/:id
   # * *Args*    :
   #   - :id -> Item id
   def create
-    @item = Item.joins(:post,:user) \
-                .joins("INNER JOIN users AS post_users on posts.user_id = users.id AND users.status = 1") \
-                .where(:id => params[:item_id], :status => 1).first!
+    @item = Item.find(params[:item_id])
 
-    if @item.user.private
-      authenticate_user!
-      if !current_user.subscribed_to(@item.user)
-        return render_error(404,"Item not found")
-      end
+    if @item.user.private && !current_user.subscribed_to(@item.user)
+      return render_error(404,"Item not found")
     end
-    
-    # Check that the item is not already in the user's set asides
-    if SetAside.where("item_id = ? AND user_id = ? AND status = 1", params[:item_id], current_user.id).first
-      return render_error(406,"Item is already in your set asides")
-    end
-    
-    @set_aside = SetAside.new({ :item_id => params[:item_id], :user_id => current_user.id })
-    if @set_aside.save
+
+    @set_aside = @item.set_asides.where(:user_id => current_user.id, :status => 1).first_or_create
+    if @set_aside.persisted?
       render :partial => 'items/item', :locals => {:item => @set_aside.item}, :status => 201
     else
-      return_error_messages(@set_asides,"Side aside save failed")
+      return_error_messages(@set_asides,"Item failed to save")
     end
   end
-  
+
   # Delete set aside
   # * *Request*    :
   #   - DELETE /set_asides/items/:id
