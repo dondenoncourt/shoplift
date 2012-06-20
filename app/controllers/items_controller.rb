7b7fbc54bed83446b3f0642c5274a9ff9e50e6e5
Aaron Bartell <aaronbartell@gmail.com>
2012-06-20 08:07:03 -0500

    Putting together join to brand for 'search' and changing parent_id to item_id for table item in various places.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,7 +8,7 @@ class ItemsController < ApplicationController
   # return:: item
   # param:: id:int - item id
   # output:: json
-  # {"id":1,"parent_id":null,"name":"Item 1","description":"Lorem ipsum dolor sit amet, aliquet feugiat. Convallis morbi fringilla gravida, phasellus feugiat dapibus velit nunc, pulvinar eget sollicitudin venenatis cum nullam, vivamus ut a sed, mollitia lectus. Nulla vestibulum massa neque ut et, id hendrerit sit, feugiat in taciti enim proin nibh, tempor dignissim, rhoncus duis vestibulum nunc mattis convallis.","brand":"Lorem ipsum dolor sit amet","retailer":"Lorem ipsum dolor sit amet","url":"http://www.jcrew.com/index.jsp","price":"1.1","comment":null,"photo_url":"http://s3.amazonaws.com/shoplift_dev/small/1/product-large-1.jpg?1337212944","hashtags_allowed":true,"created_at":"2011-12-06T09:46:45-06:00","hashtags":[{"id":1,"value":"jeans"},{"id":2,"value":"polo"},{"id":20,"value":"tie"}],"user":{"extract":{"id":1,"email":"mark@38media.net","username":"mark@38media.net","full_name":"Mark A. Roseboom","first_name":"Mark","last_name":"Roseboom","vanity_url":null,"country":null,"biography":"I like computers","notify_new_follower":null,"notify_relift":null,"notify_missing":null,"hometown":"Boston, MA","zipcode":"23238","sex":true},"sex":"Male","avatar_url_small":"http://s3.amazonaws.com/shoplift_dev/small/1/bella_4.5_mths.jpg?1337213157","avatar_url_thumb":"http://s3.amazonaws.com/shoplift_dev/thumb/1/bella_4.5_mths.jpg?1337213157"},"flag_total":1,"flags":[{"flag_type":"It contains inappropriate language"}]}
+  # {"id":1,"item_id":null,"name":"Item 1","description":"Lorem ipsum dolor sit amet, aliquet feugiat. Convallis morbi fringilla gravida, phasellus feugiat dapibus velit nunc, pulvinar eget sollicitudin venenatis cum nullam, vivamus ut a sed, mollitia lectus. Nulla vestibulum massa neque ut et, id hendrerit sit, feugiat in taciti enim proin nibh, tempor dignissim, rhoncus duis vestibulum nunc mattis convallis.","brand":"Lorem ipsum dolor sit amet","retailer":"Lorem ipsum dolor sit amet","url":"http://www.jcrew.com/index.jsp","price":"1.1","comment":null,"photo_url":"http://s3.amazonaws.com/shoplift_dev/small/1/product-large-1.jpg?1337212944","hashtags_allowed":true,"created_at":"2011-12-06T09:46:45-06:00","hashtags":[{"id":1,"value":"jeans"},{"id":2,"value":"polo"},{"id":20,"value":"tie"}],"user":{"extract":{"id":1,"email":"mark@38media.net","username":"mark@38media.net","full_name":"Mark A. Roseboom","first_name":"Mark","last_name":"Roseboom","vanity_url":null,"country":null,"biography":"I like computers","notify_new_follower":null,"notify_relift":null,"notify_missing":null,"hometown":"Boston, MA","zipcode":"23238","sex":true},"sex":"Male","avatar_url_small":"http://s3.amazonaws.com/shoplift_dev/small/1/bella_4.5_mths.jpg?1337213157","avatar_url_thumb":"http://s3.amazonaws.com/shoplift_dev/thumb/1/bella_4.5_mths.jpg?1337213157"},"flag_total":1,"flags":[{"flag_type":"It contains inappropriate language"}]}
   # ::output-end::
   # Fetch item
   # <br/><br/>Notes:<pre></pre>  
@@ -27,24 +27,17 @@ class ItemsController < ApplicationController
   end
 
   # =begin apidoc
-  # url:: /items.json
+  # url:: /items/:id/visit
   # method:: GET
   # access:: FREE
-  # return:: item
+  # return:: item URL
   # param:: id:int - item id
   # output:: json
-  # {"id":1,"parent_id":null,"name":"Item 1","description":"Lorem ipsum dolor sit amet, aliquet feugiat. Convallis morbi fringilla gravida, phasellus feugiat dapibus velit nunc, pulvinar eget sollicitudin venenatis cum nullam, vivamus ut a sed, mollitia lectus. Nulla vestibulum massa neque ut et, id hendrerit sit, feugiat in taciti enim proin nibh, tempor dignissim, rhoncus duis vestibulum nunc mattis convallis.","brand":"Lorem ipsum dolor sit amet","retailer":"Lorem ipsum dolor sit amet","url":"http://www.jcrew.com/index.jsp","price":"1.1","comment":null,"photo_url":"http://s3.amazonaws.com/shoplift_dev/small/1/product-large-1.jpg?1337212944","hashtags_allowed":true,"created_at":"2011-12-06T09:46:45-06:00","hashtags":[{"id":1,"value":"jeans"},{"id":2,"value":"polo"},{"id":20,"value":"tie"}],"user":{"extract":{"id":1,"email":"mark@38media.net","username":"mark@38media.net","full_name":"Mark A. Roseboom","first_name":"Mark","last_name":"Roseboom","vanity_url":null,"country":null,"biography":"I like computers","notify_new_follower":null,"notify_relift":null,"notify_missing":null,"hometown":"Boston, MA","zipcode":"23238","sex":true},"sex":"Male","avatar_url_small":"http://s3.amazonaws.com/shoplift_dev/small/1/bella_4.5_mths.jpg?1337213157","avatar_url_thumb":"http://s3.amazonaws.com/shoplift_dev/thumb/1/bella_4.5_mths.jpg?1337213157"},"flag_total":1,"flags":[{"flag_type":"It contains inappropriate language"}]}
+  # http://www.jcrew.com/index.jsp
   # ::output-end::
-  # Fetch item
+  # Fetch item URL
   # <br/><br/>Notes:<pre></pre>  
   # =end  
-
-
-  # Fetch item URL
-  # * *Request*    :
-  #   - GET /items
-  # * *Args*    :
-  #   - :id -> Item id
   def visit
     @item = Item.joins(:post,:user) \
                 .joins("INNER JOIN users AS post_users on posts.user_id = post_users.id AND users.status = 1") \
@@ -58,19 +51,41 @@ class ItemsController < ApplicationController
     end
   end
 
+  # =begin apidoc
+  # url:: /items/search.json?search=:search
+  # method:: GET
+  # access:: FREE
+  # return:: items list
+  # param:: search:string - value to search for in posts.name, posts.description, posts.brand or posts.retailer
+  # output:: json
+  # {"timeline":[{"id":1,"item_id":null,"name":"Item 1","description":"Lorem ipsum dolor sit amet, aliquet feugiat. Convallis morbi fringilla gravida, phasellus feugiat dapibus velit nunc, pulvinar eget sollicitudin venenatis cum nullam, vivamus ut a sed, mollitia lectus. Nulla vestibulum massa neque ut et, id hendrerit sit, feugiat in taciti enim proin nibh, tempor dignissim, rhoncus duis vestibulum nunc mattis convallis.","brand":"Lorem ipsum dolor sit amet","retailer":"Lorem ipsum dolor sit amet","url":"http://www.jcrew.com/index.jsp","price":"1.1","comment":null,"photo_url":"http://s3.amazonaws.com/shoplift_dev/small/1/product-large-1.jpg?1337212944","hashtags_allowed":true,"created_at":"2011-12-06T09:46:45-06:00","set_aside":{"created_at":"2012-06-20T12:28:44-05:00","id":18,"item_id":1,"status":1,"updated_at":"2012-06-20T12:28:44-05:00","user_id":12},"hashtags":[{"id":1,"value":"jeans"},{"id":2,"value":"polo"}],"user":{"extract":{"id":1,"email":"mark@38media.net","username":"mark@38media.net","full_name":"Mark A. Roseboom","first_name":"Mark","last_name":"Roseboom","vanity_url":null,"country":null,"biography":"I like computers","notify_new_follower":null,"notify_relift":null,"notify_missing":null,"hometown":"Boston, MA","zipcode":null,"sex":true},"sex":"Male","avatar_url_small":"http://s3.amazonaws.com/shoplift_dev/small/1/bella_4.5_mths.jpg?1337213157","avatar_url_thumb":"http://s3.amazonaws.com/shoplift_dev/thumb/1/bella_4.5_mths.jpg?1337213157","followee_count":1},"flag_total":1,"flags":[{"flag_type":"It contains inappropriate language"}]},{"id":4,"item_id":null,"name":"Item 1","description":"Lorem ipsum dolor sit amet, aliquet feugiat. Convallis morbi fringilla gravida, phasellus feugiat dapibus velit nunc, pulvinar eget sollicitudin venenatis cum nullam, vivamus ut a sed, mollitia lectus. Nulla vestibulum massa neque ut et, id hendrerit sit, feugiat in taciti enim proin nibh, tempor dignissim, rhoncus duis vestibulum nunc mattis convallis.","brand":"Lorem ipsum dolor sit amet","retailer":"Lorem ipsum dolor sit amet","url":"http://www.jcrew.com/index.jsp","price":"1.1","comment":null,"photo_url":"http://s3.amazonaws.com/shoplift_dev/small/1/product-large-1.jpg?1337212944","hashtags_allowed":true,"created_at":"2011-12-06T09:46:45-06:00","hashtags":[{"id":1,"value":"jeans"},{"id":2,"value":"polo"}],"user":{"extract":{"id":2,"email":"patrick@38media.net","username":"camachgk","full_name":"Patrick Camacho","first_name":"Patrick","last_name":"Camacho","vanity_url":"http://38media.net","country":"United States","biography":"I like dogs","notify_new_follower":true,"notify_relift":false,"notify_missing":"daily","hometown":"Waltham, MA","zipcode":"23238","sex":true},"sex":"Male","avatar_url_small":"http://s3.amazonaws.com/shoplift_dev/small/2/headless.jpg?1337213827","avatar_url_thumb":"http://s3.amazonaws.com/shoplift_dev/thumb/2/headless.jpg?1337213827","followee_count":1},"flag_total":1,"flags":[{"flag_type":"It contains inappropriate language"}]}]}
+  # ::output-end::
   # Search items
-  # * *Request*    :
-  #   - GET /items/search
-  # * *Args*    :
-  #   - :search -> Search parameter
+  # <br/><br/>Notes:<pre></pre>  
+  # =end
   def search
     @items = Item.joins(:post,:user) \
                  .joins("INNER JOIN users AS post_users on posts.user_id = post_users.id AND users.status = 1") \
-                 .where("items.status = 1 AND (posts.name LIKE '%#{params[:search]}%' OR posts.description LIKE '%#{params[:search]}%' OR posts.brand LIKE '%#{params[:search]}%') OR posts.retailer LIKE '%#{params[:search]}%'")
+                 .joins("LEFT OUTER JOIN brands AS b on posts.brand_id = b.id") \
+                 .where("items.status = 1 AND (posts.name LIKE '%#{params[:search]}%' OR posts.description LIKE '%#{params[:search]}%' OR b.name LIKE '%#{params[:search]}%') OR posts.retailer LIKE '%#{params[:search]}%'")
     render :template => 'timelines/index', :locals => {:items => @items}
   end
 
+  # =begin apidoc
+  # url:: /items.json
+  # method:: POST
+  # access:: FREE
+  # return:: item object
+  # param:: item object
+  # output:: json
+  # 
+  # ::output-end::
   # Create item
+  # <br/><br/>Notes:<pre></pre>  
+  # =end
+ 
+  # TODO finish putting curl example in here.
+  # 
   # * *Request*    :
   #   - POST /items
   # * *Args*    :
@@ -131,7 +146,7 @@ class ItemsController < ApplicationController
       return render_error(404,"Item not found")
     end
 
-    @new_item = Item.new({:user_id => current_user.id, :post_id => @item.post.id, :parent_id => @item.id, :comment => params[:comment]})
+    @new_item = Item.new({:user_id => current_user.id, :post_id => @item.post.id, :item_id => @item.id, :comment => params[:comment]})
     if @new_item.save
       @item.increment!(:relifts)
       @item.post.increment!(:relifts)
@@ -150,7 +165,7 @@ class ItemsController < ApplicationController
     authenticate_user!
     @item = Item.joins(:post,:user) \
                 .joins("INNER JOIN users AS post_users on posts.user_id = post_users.id AND users.status = 1") \
-                .where("items.id = ? AND items.user_id = ? AND ISNULL(items.parent_id) AND items.status = 1",
+                .where("items.id = ? AND items.user_id = ? AND ISNULL(items.item_id) AND items.status = 1",
                         params[:id],current_user.id).first!
 
     post = {:user_id => current_user.id}
@@ -190,7 +205,7 @@ class ItemsController < ApplicationController
 
     if @item.deactivate
       # Item is a relift and post should not be deactivated
-      if !@item.parent_id.blank?
+      if !@item.item_id.blank?
         render :json => "Item successfully deleted", :status => 200
       else
         if @item.post.deactivate
