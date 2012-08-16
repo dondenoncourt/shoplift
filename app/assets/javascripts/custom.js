970710181bccae5411e23efdef0b0a7be08d38c1
Aaron Bartell <aaronbartell@gmail.com>
2012-08-16 15:50:21 -0500

    Applying the latest UI changes from creative people.  Deleted images out of app/assets/images because the app is always pulling from public/images (except for "missing" avatar images).

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -2,104 +2,133 @@
 
 // Drop Down
 $(document).ready(function(){
-	$('blockquote p').wrapInner('<span>');
-	$('.productBox:nth-child(3n)').addClass('active');
+  $('blockquote p').wrapInner('<span>');
+  $('.productBox:nth-child(3n)').addClass('active');
 
-$('.profilePan #userImage').hover(
+$('.profilePan img').hover(
 function () {
-	$(this).addClass('active');
-	//$(this).next('ul').fadeIn('fast');
-	$('#mainMenu').fadeIn('fast');
+  $(this).addClass('active');
+  $(this).next('ul').fadeIn('fast');
 }
 );
 $('body, html').click(
 function () {
-	$(this).removeClass('active');
-	//$('.profilePan img').next('ul').fadeOut();
-	$('#mainMenu').fadeOut();
+  $(this).removeClass('active');
+  $('.profilePan img').next('ul').fadeOut();
 }
 );
 
 $('.arrowLink').hover(
 function () {
-	$(this).addClass('active');
-	$(this).children('ul').fadeIn('fast');
+  $(this).addClass('active');
+  $(this).children('ul').fadeIn('fast');
 },
 function () {
-	$(this).removeClass('active');
-	$(this).children('ul').fadeOut();
+  $(this).removeClass('active');
+  $(this).children('ul').fadeOut();
 }
 );
 });
 
 // Slider
 function highlight( items ) {
-	items.filter(":eq(1)").addClass("active");
-}
+  items.filter(":eq(1)").addClass("active");
+} 
 function unhighlight( items ) {
-	//items.addClass("inactive"),
-	items.removeClass("active");
+  //items.addClass("inactive"),
+  items.removeClass("active");
 }
 
-
-var lastPage = 0;
-var currentPage = 1;
 $(function() {
-  lastPage = $('#lastPage').html();
-	$('#results.carousel').carouFredSel({					
-		width: '100%',
-    infinite: true,
-		items: 3,
-		scroll: {
-			items: 1,
-			play: false
-		},
-    auto	: {
+  $('#carousel').carouFredSel({         
+    width: '100%',
+    items: 3,
+    auto : false,
+    scroll: {
+      items: 1,
+      duration: 500,
+      pauseDuration: 5000,
+      //pauseOnHover    : true
+    },
+    prev: {
       onBefore: function(oldItems, newItems) {
-        expandItems();
         unhighlight( oldItems );
       },
-      onAfter	: function(oldItems, newItems) {
+      onAfter : function(oldItems, newItems) {
         highlight( newItems );
       }
     },
-		prev: {
-			onBefore: function(oldItems, newItems) {
-				unhighlight( oldItems );
-			},
-			onAfter	: function(oldItems, newItems) {
-				highlight( newItems );
-			}
-		},
-		next: {
-			onBefore: function(oldItems, newItems) {
-        expandItems();
-				unhighlight( oldItems );
-			},
-			onAfter	: function(oldItems, newItems) {
-				highlight( newItems );
-			}
-		}
-	});
-	$("#next").click(function() {
-		$("#results.carousel").trigger("next", 1);
-	});
-	$("#prev").click(function() {
-		$("#results.carousel").trigger("prev", 1);
-	});
-});
-function expandItems() {
-  currentPage++;
-  if (currentPage <= lastPage) {
-    $.ajax({
-      url:'/timelines?page='+currentPage,
-      type:'GET',
-      success:function(data) {
-        $('#results').append(data);
+    next: {
+      onBefore: function(oldItems, newItems) {
+        unhighlight( oldItems );
       },
-      error:function(xhr,textStatus,errorThrown){
-        alert(xhr.responseText);
+      onAfter : function(oldItems, newItems) {
+        highlight( newItems );
       }
-    });
-  }
-}
+    }
+  });
+  $("#next").click(function() {
+    $("#carousel").trigger("next", 1);
+    $('.productBox2').animate({ width: 'hide' });
+    $('.productBox').animate({width:"545px"});
+    $('.slideBtn').removeClass('slideOff');
+  });
+  $("#prev").click(function() {
+    $("#carousel").trigger("prev", 1);
+    $('.productBox2').animate({ width: 'hide' });
+    $('.productBox').animate({width:"545px"});
+    $('.slideBtn').removeClass('slideOff');
+  });
+  
+  
+//  $(".slide").click(function() {
+//     $(this).addClass('slideOff');
+//     $(this).removeClass('slide');
+//     $(this).next('.productBox2').animate({ width: 'show' });
+//  }
+//  );
+//  $(".slideOff").click(function() {
+//     $(this).removeClass('slideOff');
+//     $(this).addClass('slide');
+//     $(this).next('.productBox2').animate({ width: 'hide' });
+//  }
+//  );
+
+  // Right Slide Toggle
+  var clickevent = true;
+    $(".slideBtn").click(function() {
+      if(clickevent == true) {
+      $(this).addClass('slideOff');
+      $(this).next('.productBox2').fadeIn('fast');
+      $(this).next('.productBox2').animate({"right": "14" , "z-index": "0"},1000);
+      $(this).parent('.productBox').animate({width:"750px"},1000 , function() {
+});
+      
+      
+      
+      clickevent = false;
+      return false;
+    } else {
+      $(this).next('.productBox2').animate({"z-index": "-5"},1);
+      $(this).removeClass('slideOff');    
+      $(this).next('.productBox2').animate({"right": "0"},1000);
+      $(this).parent('.productBox').animate({width:"545px"},1000);
+      $(this).next('.productBox2').fadeOut(200);
+      clickevent = true;
+      return false;
+    }
+  });
+
+
+  //Tab Show Hide
+  $('.tabContent1').show();
+  $('.tabNav li:first-child').addClass('activeTab');
+  $('.tabNav li').click(function(){
+    var divId = "." + $(this).attr('rel');
+      $(this).parent().siblings('div.tabContent').hide();
+      $(this).parent().siblings(divId).fadeIn('fast');
+      $(this).siblings().removeClass('activeTab');
+      $(this).addClass('activeTab');
+      return false;
+  });
+});
