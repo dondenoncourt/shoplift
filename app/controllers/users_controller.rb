e41767be199b8d7e59749e894e64f8febf128e99
Don Denoncourt <dondenoncourt@gmail.com>
2012-08-21 10:40:02 -0400

    more doc for shoplifters api for trending etc.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
   # return:: list of users
   # param:: page:int - the page, default is 1 (optional)
   # param:: per_page:int - max items per page, default is 2 (optional)
+  # param:: option:trending|popular|recommeded|staff_picks|local_favorites (optional)
   # output:: json
   # [{"avatar_content_type":"image/jpeg","avatar_file_name":"bella_4.5_mths.jpg","avatar_file_size":151209,"avatar_updated_at":"2012-05-16T19:05:57-05:00","biography":"I like computers","birthdate":"1987-05-24","count_of_followers":1,"count_of_hashtags":0,"count_of_posts":0,"country":null,"created_at":"2011-12-19T23:04:53-06:00","email":"mark@38media.net","first_name":"Mark","full_name":"Mark A. Roseboom","hometown":"Boston, MA","id":1,"last_name":"Roseboom","latitude":null,"longitude":null,"missing":null,"notify_missing":null,"notify_new_follower":null,"notify_relift":null,"private":false,"sex":true,"status":1,"updated_at":"2012-02-09T10:50:43-06:00","url":"http://38media.net","username":"mroseboom","vanity_url":null,"zipcode":null}]
   # ::output-end::
