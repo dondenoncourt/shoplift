f4521be3ce0fcdc735c477fefa3d51a23561b075
Don Denoncourt <dondenoncourt@gmail.com>
2012-08-22 14:41:43 -0400

    add subscriptions to docs

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,21 +1,33 @@
 class SubscriptionsController < ApplicationController
   before_filter :authenticate_user!, :except => [:show]
 
+  # =begin apidoc
+  # url:: /subscriptions
+  # method:: GET
+  # access:: FREE
+  # return:: Fetch all current users subscriptions
+  # output:: json
+  # [{"subscriptions":[{"extract":{"id":2,"email":"patrick@elsewhere.net","username":"patrick@elsewhere.net","full_name":"Patrick Camacho","first_name":"Patrick","last_name":"Camacho","url":"http://38media.net","vanity_url":"http://38media.net","country":"United States","biography":"I like dogs","notify_new_follower":true,"notify_relift":false,"notify_missing":"daily","hometown":"Waltham, MA","zipcode":"23238","sex":true},"sex":"Male","avatar_url_small":"http://s3.amazonaws.com/shoplift_dev/small/2/headless.jpg?1337213827","avatar_url_thumb":"http://s3.amazonaws.com/shoplift_dev/thumb/2/headless.jpg?1337213827","followee_count":3},{"extract":{"id":6,"email":"mike@zstudiolabs.com","username":"mike@zstudiolabs.com","full_name":"Mike","first_name":null,"last_name":null,"url":"http://www.ZStudioLabs.com","vanity_url":null,"country":"United States","biography":"Professional scuba diver, amateur rock climber.","notify_new_follower":true,"notify_relift":true,"notify_missing":"daily","hometown":null,"zipcode":"83703","sex":true},"sex":"Male","avatar_url_small":"http://s3.amazonaws.com/shoplift_dev/small/6/Mike.jpg?1338540852","avatar_url_thumb":"http://s3.amazonaws.com/shoplift_dev/thumb/6/Mike.jpg?1338540852","followee_count":3},{"extract":{"id":7,"email":"brandon@theshoplift.com","username":"brandon@theshoplift.com","full_name":"Brandon Fail","first_name":null,"last_name":null,"url":"www.theshoplift.com","vanity_url":null,"country":"United States","biography":"Here's my blurb everybody!  ","notify_new_follower":true,"notify_relift":true,"notify_missing":"daily","hometown":null,"zipcode":"10014","sex":true},"sex":"Male","avatar_url_small":"/assets/avatars/small/missing.png","avatar_url_thumb":"/assets/avatars/thumb/missing.png","followee_count":3}]}]
+  # ::output-end::
   # Fetch all current users subscriptions
-  # * *Request*    :
-  #   - GET /subscriptions
-  # * *Args*    :
-  #   - void
+  # <br/><br/>Notes:<pre>curl -X GET -u mark@elsewhere.net:vo2max localhost:3000/subscriptions/users/1</pre>
+  # =end
   def index
     params[:id] = current_user.id
     return show
   end
 
-  # Fetch all subscriptions for specified user
-  # * *Request*    :
-  #   - GET /subscriptions/users/:id
-  # * *Args*    :
-  #   - :id -> User id
+  # =begin apidoc
+  # url:: /subscriptions/users/:id
+  # method:: GET
+  # access:: FREE
+  # return:: list of users that the passed id is following
+  # output:: json
+  # [{"subscriptions":[{"extract":{"id":2,"email":"patrick@elsewhere.net","username":"patrick@elsewhere.net","full_name":"Patrick Camacho","first_name":"Patrick","last_name":"Camacho","url":"http://38media.net","vanity_url":"http://38media.net","country":"United States","biography":"I like dogs","notify_new_follower":true,"notify_relift":false,"notify_missing":"daily","hometown":"Waltham, MA","zipcode":"23238","sex":true},"sex":"Male","avatar_url_small":"http://s3.amazonaws.com/shoplift_dev/small/2/headless.jpg?1337213827","avatar_url_thumb":"http://s3.amazonaws.com/shoplift_dev/thumb/2/headless.jpg?1337213827","followee_count":1}]}]
+  # ::output-end::
+  # Get a list of all the users that passed id's user is following.
+  # <br/><br/>Notes:<pre>curl -X GET -u mark@elsewhere.net:vo2max localhost:3000/subscriptions/users/1</pre>
+  # =end
   def show
     @user = User.where(:id => params[:id], :status => 1).first!
     if @user.private
@@ -30,11 +42,18 @@ class SubscriptionsController < ApplicationController
     render :show
   end
 
-  # Subscribed to user
-  # * *Request*    :
-  #   - POST /subscriptions
-  # * *Args*    :
-  #   - :user_id -> User id
+  # =begin apidoc
+  # url:: /subscriptions/:id
+  # method:: POST
+  # access:: FREE
+  # return:: users that user is following
+  # param:: user_id:int - the id of the user to follow
+  # output:: json
+  # {"extract":{"id":2,"email":"patrick@elsewhere.net","username":"patrick@elsewhere.net","full_name":"Patrick Camacho","first_name":"Patrick","last_name":"Camacho","url":"http://38media.net","vanity_url":"http://38media.net","country":"United States","biography":"I like dogs","notify_new_follower":true,"notify_relift":false,"notify_missing":"daily","hometown":"Waltham, MA","zipcode":"23238","sex":true},"sex":"Male","avatar_url_small":"http://s3.amazonaws.com/shoplift_dev/small/2/headless.jpg?1337213827","avatar_url_thumb":"http://s3.amazonaws.com/shoplift_dev/thumb/2/headless.jpg?1337213827","followee_count":9}
+  # ::output-end::
+  # Start following (a.k.a. subscribe to) a user
+  # <br/><br/>Notes:<pre>curl -X POST -u mark@elsewhere.net:vo2max localhost:3000/subscriptions.json?user_id=2</pre>
+  # =end
   def create
     user = User.find(params[:user_id])
     status = user.private ? 2 : 1
@@ -48,11 +67,18 @@ class SubscriptionsController < ApplicationController
     end
   end
 
-  # Unsubscribe
-  # * *Request*    :
-  #   - DELETE /subscriptions/users/:id
-  # * *Args*    :
-  #   - :id -> User id
+  # =begin apidoc
+  # url:: /subscriptions/users/:id
+  # method:: DELETE
+  # access:: FREE
+  # return:: followee count
+  # param:: user_id:int - the id of the user to stop following
+  # output:: json
+  # [  {"followee_count":8}  ]
+  # ::output-end::
+  # Stop following (a.k.a. unsubscribe to) a user
+  # <br/><br/>Notes:<pre>curl -X DELETE -u mark@elsewhere.net:vo2max localhost:3000/subscriptions/users/2</pre>
+  # =end
   def destroy
     subscription = Subscription.where(:user_id => params[:id], :follower_id => current_user.id).first
 
