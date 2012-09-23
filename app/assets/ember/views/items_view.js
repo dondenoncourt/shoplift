022f8b5abc024589ef7d25c3519cba7f52ec4b60
David Mazza <github@dmzza.com>
2012-09-23 18:56:04 -0400

    Cleanup Item View, separated inview binding as a Mixin for reusability

--- app/assets/ember/views/items_view.js
+++ app/assets/ember/views/items_view.js
@@ -1,4 +1,21 @@
-Shoplift.ItemView = Ember.View.extend({
+Shoplift.InViewMixin = Ember.Mixin.create({
+	isInView: false,
+	inViewDidInsert: function() {
+		var that = this;
+		
+		this.$().on('inview', function(event, isInView, visiblePartX, visiblePartY) {
+			if(isInView) {
+				that.set("isInView", true);
+			}
+		});
+	},
+	inViewWilDestroy: function() {
+		this.$().off("inview");
+	}
+})
+
+
+Shoplift.ItemView = Ember.View.extend(Shoplift.InViewMixin, {
   templateName: 'item',
   classNames: ['product', 'item'],
 
@@ -25,20 +42,11 @@ Shoplift.ItemView = Ember.View.extend({
   drawerIsOpen: false,
   drawerIsAnimating: false,
   
-  didInsertElement: function(ev) {	 
-  	var that = this,
-		  	time = 800
-		  	drawerIsAnimating = that.get("drawerIsAnimating"),
-		  	drawerIsOpen = that.get("drawerIsOpen");
-		  	
-  	this.$().bind('inview', function(event, isInView, visiblePartX, visiblePartY) {
-  		if(isInView) {
-  			that.set("isInView", true);
-  		}
-  	});
-  	 
-  	  
-	  // Right Drawer Toggle
+  initDrawer: function() {
+	  var that = this,
+	  time = 800
+	  drawerIsAnimating = that.get("drawerIsAnimating"),
+	  drawerIsOpen = that.get("drawerIsOpen");
 	  
 	  this.$().on("click", ".product-sub-container", function(e) {
 	  	e.preventDefault();
@@ -48,6 +56,7 @@ Shoplift.ItemView = Ember.View.extend({
 	  		toggleDrawer(drawer, container);
 	  	}
 	  });
+	  
 	  this.$().on("click", ".drawer-opener", function(e) {
 	  	e.preventDefault();
 	  	if(!drawerIsAnimating) {
@@ -64,49 +73,60 @@ Shoplift.ItemView = Ember.View.extend({
 	  			drawerIsOpen = that.get("drawerIsOpen");
 	  
 	  	if(!drawerIsAnimating) {
-				that.set("drawerIsAnimating", true);
+	  		that.set("drawerIsAnimating", true);
 	  	} else {
-				return;
-		  }
+	  		return;
+	  	}
 	  	
 	  	if(drawerIsOpen) {
-		  	drawer.
-		  		delay(62).
-		  			transition({left: '-170px'}, 300, 'out');
-		  } else {
-		  	drawer.
-		  		transition({left: '0'}, 300, 'out');
-		  }
-		  
-		  drawer.toggleClass("drawer-hidden");
-		  
-		  if(drawerIsOpen) {
-		  	container.
-		  		animate({width:"394px"}, 600, 'easeOutBack').
-		  		animate({width:"374px"}, 200, 'linear');
-		  } else {
-		  	container.
-		  		delay(100).
-		  			animate({width:"547px"}, 500, 'easeOutBack').
-		  			animate({width:"567px"}, 200, 'linear');
-		  }
-		  		
-		  if(drawerIsOpen) {
-			  angle = '0deg';
-		  } else {
-		  	angle = '+45deg';
-		  }
-		  
-		  opener.transition({rotate: angle}, time, function() { 
-		  	that.set("drawerIsOpen", !drawerIsOpen); 
-		  	that.set("drawerIsAnimating", false);
-		  });
+	  		drawer.
+	  			delay(62).
+	  				transition({left: '-170px'}, 300, 'out');
+	  	} else {
+	  		drawer.
+	  			transition({left: '0'}, 300, 'out');
+	  	}
+	  	
+	  	drawer.toggleClass("drawer-hidden");
+	  	
+	  	if(drawerIsOpen) {
+	  		container.
+	  			animate({width:"394px"}, 600, 'easeOutBack').
+	  			animate({width:"374px"}, 200, 'linear');
+	  	} else {
+	  		container.
+	  			delay(100).
+	  				animate({width:"547px"}, 500, 'easeOutBack').
+	  				animate({width:"567px"}, 200, 'linear');
+	  	}
+	  			
+	  	if(drawerIsOpen) {
+	  		angle = '0deg';
+	  	} else {
+	  		angle = '+45deg';
+	  	}
+	  	
+	  	opener.transition({rotate: angle}, time, function() { 
+	  		that.set("drawerIsOpen", !drawerIsOpen); 
+	  		that.set("drawerIsAnimating", false);
+	  	});
 	  };
+
   },
-  /*willDestroyElement: function() {
-	  this.$(".product-sub-container").off("click");
-	  this.$(".drawer-opener").off("click");
-  }*/
+  
+  cleanupDrawer: function() {
+	  this.$().off("click");
+  },
+  
+  didInsertElement: function(ev) {	
+  	this.inViewDidInsert();
+  	this.initDrawer();
+  },
+  
+  willDestroyElement: function() {
+  	this.inViewWillDestroy();
+  	this.cleanupDrawer();
+  }
 });
 
 Shoplift.ItemReliftView = Ember.View.extend({
