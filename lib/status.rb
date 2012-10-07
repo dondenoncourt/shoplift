8e21420b8d6e4d42cf9724e23296c6a8fadec65b
David Mazza <github@dmzza.com>
2012-10-06 21:30:28 -0400

    status..

--- lib/status.rb
+++ lib/status.rb
@@ -1,29 +1,22 @@
-module StatusModule
-  def self.included(recipient)
-    recipient.class_eval do
-      include ModelInstanceMethods
-    end
+module Status
+  DEACTIVATED = 0
+  ACTIVATED   = 1
+  SUSPENDED   = 2
+
+  def deactivate
+    self.update_attributes(status: DEACTIVATED)
   end
-  
-  # Instance Methods
-  module ModelInstanceMethods
-    
-    def deactivate
-      self.update_attributes(:status => 0)
-    end
-    
-    def suspend
-      self.update_attributes(:status => 2)
-    end
-    
-    def activate
-      self.update_attributes(:status => 1)
-    end
-    
+
+  def suspend
+    self.update_attributes(status: SUSPENDED)
+  end
+
+  def activate
+    self.update_attributes(status: ACTIVATED)
   end
 end
 
 #This is where your module is being included into ActiveRecord
 if Object.const_defined?("ActiveRecord")
-  ActiveRecord::Base.send(:include, StatusModule)
+  ActiveRecord::Base.send(:include, Status)
 end
