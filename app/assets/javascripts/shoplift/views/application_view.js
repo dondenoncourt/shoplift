d09e34ddd1c9fbeabf7d69bdeb0cfab1c007b240
David Mazza <github@dmzza.com>
2012-10-09 15:41:36 -0400

    More work to ensure that the coming soon popup behaves correctly

--- app/assets/javascripts/shoplift/views/application_view.js
+++ app/assets/javascripts/shoplift/views/application_view.js
@@ -2,13 +2,13 @@ Shoplift.ApplicationView = Ember.View.extend({
   templateName: 'shoplift/templates/application',
   setupComingSoon: function() {
     this.$('.coming-soon').on("mouseover", function(e) {
-      if(!$(e.fromElement).hasClass("coming-soon") && !$(e.fromElement).hasClass("coming-soon-popup")) {
+      if(e.fromElement === null || (!$(e.fromElement).hasClass("coming-soon-popup") && e.fromElement !== e.currentTarget)) {
         $(this).append('<div class="coming-soon-popup fade-in-fast">Coming soon!</div>');
       }
     });
     this.$('.coming-soon').on("mouseout", function(e) {
       var that = this;
-      if(!$(e.relatedTarget).hasClass("coming-soon-popup") && !$(e.relatedTarget).hasClass("coming-soon")) {
+      if(!$(e.relatedTarget).hasClass("coming-soon-popup") && e.relatedTarget !== e.currentTarget) {
         $(this).children(".coming-soon-popup").removeClass('fade-in-fast');
         $(this).children(".coming-soon-popup").addClass('fade-out-fast');
         setTimeout(function() {
