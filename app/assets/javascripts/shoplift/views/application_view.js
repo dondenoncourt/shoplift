b22dd743943547abe336fcd01e1b9d63a329a780
David Mazza <github@dmzza.com>
2012-10-09 15:12:36 -0400

    Removed twbs tooltip, replaced with our own coming soon popup
    Appended to any .coming-soon and absolutely positioned slightly higher
    than the element. Always hovers on top, and is usually centered over
    the object if it is typically positioned within it's own parent.
    
    Lots of work done to ensure that mouse overs and outs between the
    .coming-soon or .coming-soon-popup don't trigger extra fade-in's or
    out's.

--- app/assets/javascripts/shoplift/views/application_view.js
+++ app/assets/javascripts/shoplift/views/application_view.js
@@ -1,12 +1,30 @@
 Shoplift.ApplicationView = Ember.View.extend({
   templateName: 'shoplift/templates/application',
-  didInsertElement: function() {
-    this.$('.coming-soon').tooltip({
-      title: 'Coming soon!'
+  setupComingSoon: function() {
+    this.$('.coming-soon').on("mouseover", function(e) {
+      if(!$(e.fromElement).hasClass("coming-soon") && !$(e.fromElement).hasClass("coming-soon-popup")) {
+        $(this).append('<div class="coming-soon-popup fade-in-fast">Coming soon!</div>');
+      }
     });
-    this.$('.coming-soon').on("hover", function() {
-      $(this).tooltip();
-      console.log("hello");
-    })
+    this.$('.coming-soon').on("mouseout", function(e) {
+      var that = this;
+      if(!$(e.relatedTarget).hasClass("coming-soon-popup") && !$(e.relatedTarget).hasClass("coming-soon")) {
+        $(this).children(".coming-soon-popup").removeClass('fade-in-fast');
+        $(this).children(".coming-soon-popup").addClass('fade-out-fast');
+        setTimeout(function() {
+          $(that).children(".coming-soon-popup").remove();
+        }, 200);
+      }
+    });
+  },
+  destroyComingSoon: function() {
+    this.$(".coming-soon").off("mouseover");
+    this.$(".coming-soon").off("mouseout");
+  },
+  didInsertElement: function() {
+    this.setupComingSoon();
+  },
+  willDestroyElement: function() {
+    this.destroyComingSoon()
   }
 });
